5408b8e7d4251662fa968205170e74dae4343dd8
AlphaArslan <arslan.alpha21@gmail.com>
2019-10-31 23:12:42 +0200

    routing works

--- webpage/server.py
+++ webpage/server.py
@@ -1,1 +1,30 @@
-import flask
+########################## import
+from flask import Flask, render_template
+
+
+########################## setup
+app = Flask(__name__)
+
+
+
+########################## routes
+@app.route('/')
+def home():
+    return render_template("index.html")
+
+@app.route('/stream')
+def strem():
+    return render_template("stream.html")
+
+@app.route('/log')
+def log():
+    return render_template("log.html")
+
+@app.route('/users')
+def users():
+    return render_template("users.html")
+
+
+########################## main
+if __name__ == '__main__':
+    app.run(debug=True)
