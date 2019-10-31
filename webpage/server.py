86fcd3bd789ac12f3bd8941c9e6fd4e9f96fd9df
AlphaArslan <arslan.alpha21@gmail.com>
2019-10-31 23:58:44 +0200

    users page is fully working

--- webpage/server.py
+++ webpage/server.py
@@ -1,9 +1,18 @@
 ########################## import
-from flask import Flask, render_template
-
+from flask import Flask, render_template, redirect, request
+import glob
+import os
 
 ########################## setup
 app = Flask(__name__)
+
+########################## file path
+slash = "\\"         # windows
+# slash = "/"          # linux
+
+
+known_path = "static" + slash + "known_ppl" + slash
+unknown_path = "static" + slash + "unknown_ppl" + slash
 
 
 
@@ -22,7 +31,31 @@ def log():
 
 @app.route('/users')
 def users():
-    return render_template("users.html")
+    known_list = []
+    for user in sorted(glob.glob(known_path + '*')):
+        name = user.split(slash)[-1].split('.')[0]
+        known_list.append(name)
+    unknown_list = []
+    for user in sorted(glob.glob(unknown_path + '*')):
+        name = user.split(slash)[-1].split('.')[0]
+        unknown_list.append(name)
+    return render_template("users.html", known_list = known_list, unknown_list = unknown_list)
+
+
+@app.route('/remove/<string:k>/<string:name>')
+def remove(k, name):
+    if k == 'known':
+        os.remove(known_path+name+'.jpg')
+    elif k == 'unknown':
+        os.remove(unknown_path+name+'.jpg')
+    return redirect('/users')
+
+
+@app.route('/add_user/<string:old_name>', methods=['GET', 'POST'])
+def add_user(old_name):
+    name = request.form["name"]
+    os.rename(unknown_path+old_name+'.jpg',known_path+name+'.jpg')
+    return redirect('/users')
 
 
 ########################## main
